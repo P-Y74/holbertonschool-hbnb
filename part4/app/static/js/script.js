120c47769dfd29a9591b5dfcd1dc119f6ff3ed7b
P-Y74 <197254317+P-Y74@users.noreply.github.com>
2025-07-23 15:21:08 +0200

    feat(auth): add logout functionality and fetch places after login
    - Implemented logout button handling: clears token cookie and redirects to login page
    - Updated authentication check to toggle login/logout display
    - Added fetchPlaces function to retrieve places data after successful authentication

--- part4/app/static/js/script.js
+++ part4/app/static/js/script.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
   const loginForm = document.getElementById('login-form');
 
+
   if (loginForm) {
     loginForm.addEventListener('submit', async (event) => {
       event.preventDefault();
@@ -19,7 +20,9 @@ document.addEventListener('DOMContentLoaded', () => {
       }
     });
   }
+  checkAuthentication();
 });
+
 
 async function loginUser(email, password) {
   const response = await fetch('http://localhost:5000/api/v1/auth/login', {
@@ -37,4 +40,61 @@ async function loginUser(email, password) {
   } else {
     alert('Login failed: ' + response.statusText);
   }
+}
+
+
+function checkAuthentication() {
+  const token = getCookie('token');
+  const loginLink = document.getElementById('login-link');
+  const logoutButton = document.getElementById('logout-button');
+
+  if (logoutButton) {
+    logoutButton.addEventListener('click', () => {
+      document.cookie = "token=; path=/; expires=Thu, 01 Jan 1970 00:00:00 UTC;"
+      window.location.href = 'login';
+    });
+  }
+
+  if (!token) {
+    loginLink.style.display = 'block';
+    logoutButton.style.display = 'none';
+  } else {
+    loginLink.style.display = 'none';
+    logoutButton.style.display = 'block';
+
+    // Fetch places data if the user is authenticated
+    fetchPlaces(token);
+  }
+
+
+}
+function getCookie(name) {
+  // Function to get a cookie value by its name
+  const cookieString = document.cookie;
+  const cookies = cookieString.split(';');
+  for (let i = 0; i < cookies.length; i++) {
+    const cookie = cookies[i].trim();
+    if (cookie.startsWith(name + '=')) {
+      return cookie.substring(name.length + 1);
+    }
+  }
+  return null;
+}
+
+
+async function fetchPlaces(token) {
+  // Make a GET request to fetch places data
+  try {
+    const response = await fetch('http://localhost:5000/api/v1/places');
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error('Error:', error);
+  }
+  // Include the token in the Authorization header
+
+  // Handle the response and pass the data to displayPlaces function
 }
