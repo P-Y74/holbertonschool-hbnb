ee3ea4c9919567ae9ad1a501d00a7ceb9a09070a
P-Y74 <197254317+P-Y74@users.noreply.github.com>
2025-07-24 12:13:24 +0200

    fix: correct db import to avoid circular import

--- part4/app/services/repositories/amenity_repository.py
+++ part4/app/services/repositories/amenity_repository.py
@@ -1,4 +1,4 @@
-from app import db
+from app.extensions import db
 from app.models.amenity import Amenity
 from datetime import datetime, timezone
 from app.persistence.repository import SQLAlchemyRepository
